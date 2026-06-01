410fc6b8e34100118624274a373730bed9565a9a
phanmemquanli <tulieuhopthanh@gmail.com>
2026-06-01 13:12:03 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -1,7 +1,7 @@
 // CẤU HÌNH HỆ THỐNG GIAO TIẾP VÀ HIỂN THỊ (CLIENT)
 // ===========================================================================
 const HKS_CAU_HINH_WEBAPP_STR = "https://script.google.com/macros/s/AKfycbwY_LDKefb80LVnegm_IwoSWzvpAuZB7cLqefApUW4eDiYG3Qc4JYqjLgoJT8SRKG9W/exec"; 
-const HKS_CAU_HINH_CLIENT_ID_STR = "407480994586-m6fpq6sfcc90qqj9k08rsmi1lge6br94.apps.googleusercontent.com";
+const HKS_CAU_HINH_CLIENT_ID_STR = "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com";
 const HKS_CAU_HINH_LOGO_STR = "https://i.ibb.co/d0JNW94Z/logo-l-a-ch-n6.png";
 const HKS_CAU_HINH_TEN_DINH_DANH_STR = "Trường TH&THCS Hợp Thành";
 const HKS_CAU_HINH_TAC_GIA_STR = "Hoàng Ngọc Lâm";
